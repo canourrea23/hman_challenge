b5eb7f5fc0300c7ebf75e32cd805d42ec6844c2c
Carolina Urrea <73137943+canourrea23@users.noreply.github.com>
2021-06-22 20:24:55 -0500

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,143 +1,153 @@
+from word import word_list
 import random
-from word import words
 
 
 def get_word():
-    word = random.choice(words)
+    word = random.choice(word_list)
     return word.upper()
 
-
-def play(world):
-    word_completion = "_" * len(words)
+def play(word):
+    word_completion = '_' * len(word)
     guessed = False
     guessed_letters = []
-    guessed_words = []
-    tries = 7
-    print("Let's play Hangman!")
+    guessed_words = [] 
+    tries = 6
+    print("Let's play Hangman!")    
     print(display_hangman(tries))
     print(word_completion)
-    print("\n")
+    print('\n')
     while not guessed and tries > 0:
-        guess = input('Please guess a letter or word: ').upper()
+        guess = input("Please guess a letter or word: ").upper()
         if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letters:
                 print('You already guessed the letter', guess)
-            elif guess not in words:
-                print(guess, "is not in the word.")
+            elif guess not in word:
+                print(guess, 'is not in the word.')    
                 tries -= 1
                 guessed_letters.append(guess)
             else:
-                print("Good Job,", guess, "is in the word.")
-                guessed_letters.append(guess)
-                word_as_list = list(word_completion)
-                indices = [i for i, letter in enumerate(
-                    words) if letter == guess]
-            for index in indices:
-                word_as_list[index] = guess
-            word_completion = ''.join(word_as_list)
-            if '_' not in word_completion:
-                guessed = True
-        elif len(guess) == len(words) and guess.isalpha():
+                print("Good job,", guess, "is in the word!")
+                guessed_letters.append(guess)  
+                word_as_list = list(word_completion)  
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True    
+
+
+        elif len(guess) == len(word) and guess.isalpha():
             if guess in guessed_words:
                 print("You already guessed the word", guess)
-            elif guess != words:
+            elif guess != word:
                 print(guess, "is not the word.")
                 tries -= 1
                 guessed_words.append(guess)
             else:
                 guessed = True
-                word_completion = words
+                word_completion = word        
+
+
         else:
-            print("Not a valid guess.")
+            print('Not a valid guess.')    
         print(display_hangman(tries))
         print(word_completion)
-        print("\n")
+        print('\n') 
     if guessed:
-        print("Congrats, you guessed the word! You win!")
+        print("Congrats, you win!") 
     else:
-        print("Sorry, you ran out of tries. The word was " +
-              words + ". Maybe next time!")
+        print("Sorry, you ran out of tries. The word was " + word + ", maybe next time")          
+
+
+
+
+
+
 
 
 def display_hangman(tries):
-    hangman = [
-        """
-    ____
-    |     |
-    |     O
-    |   - | -
-    |    /\
-    |
-    -
-    """,
-
-        """
-    ____
-    |     |
-    |     O
-    |   - | -
-    |    /
-    |
-    -
-    """,
-        """
-    ____
-    |     |
-    |     O
-    |   - | -
-    |    
-    |
-    -
-    """,
-        """
-    ____
-    |     |
-    |     O
-    |   - | 
-    |    
-    |
-    -
-    """,
-        """
-    ____
-    |     |
-    |     O
-    |   - 
-    |    
-    |
-    -
-    """,
-        """
-    ____
-    |     |
-    |     O
-    |   
-    |    
-    |
-    -
-    """,
-        """
-    ____
-    |     |
-    |    
-    |   
-    |    
-    |
-    -
-    """
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
     ]
-    print(len(hangman))
-    print('hello', tries)
-    return hangman[tries - 1]
+    return stages[tries]
 
 
 def main():
     word = get_word()
     play(word)
-    while input('Play Again? (Y/N) ').upper() == 'Y':
-        words = get_word()
-        play(words)
+    while input("Play Again? (Y/N) ").upper =='Y':
+        word = get_word()
+        play(word)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    main()     
