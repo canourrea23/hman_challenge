9307dd8fc499bf9036c507c4f6eaa00557cba005
Carolina Urrea <73137943+canourrea23@users.noreply.github.com>
2021-06-22 20:25:42 -0500

    Update word.py

--- word.py
+++ word.py
@@ -1,1 +1,3 @@
-word_list = ['study', 'code',  'project', 'read', 'debug']
+# word_list = ['study', 'code',  'project', 'read', 'debug']
+word_list = ["pizza", "cheese", "pepperoni", "bacon",
+         "sausage", "peppers", "mozzarella", "sauce"]
